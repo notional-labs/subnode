246d0f65e6a00d5125eb145191c398dc2d250b0c
Tuan Pham Anh <baabeetaa@gmail.com>
2023-04-07 17:43:48 +0700

    support /block_search partially (#29)
    https://github.com/notional-labs/subnode/issues/28

--- aggregator.go
+++ aggregator.go
@@ -11,6 +11,34 @@ import (
 // the logic here is it will iterate all the subnodes until result is found
 // https://github.com/notional-labs/subnode/issues/20
 
+func DoAggeratorJsonRpcOverHttp_block_search(w http.ResponseWriter, jsonBody []byte) {
+	for i, s := range PoolRpc {
+		rpcUrl := s.Backend.Rpc
+		body, err := FetchJsonRpcOverHttp(rpcUrl, jsonBody)
+		if err == nil {
+			var j0 interface{}
+			err = json.Unmarshal(body, &j0)
+			if err != nil {
+				SendError(w)
+				return
+			}
+
+			if m0, ok := j0.(map[string]interface{}); ok {
+				if result, ok := m0["result"].(map[string]interface{}); ok {
+					if blocks, ok := result["blocks"].([]interface{}); ok {
+						if (len(blocks) > 0) || (i >= len(PoolRpc)-1) { // found result or last node, send it
+							SendResult(w, body)
+							return
+						}
+					}
+				}
+			}
+		}
+	}
+
+	SendError(w)
+}
+
 func DoAggeratorJsonRpcOverHttp_tx(w http.ResponseWriter, jsonBody []byte) {
 	for i, s := range PoolRpc {
 		rpcUrl := s.Backend.Rpc
@@ -134,6 +162,34 @@ func DoAggeratorUriOverHttp_block_by_hash(w http.ResponseWriter, strQuery string
 					if (result["block"] != nil) || (i >= len(PoolRpc)-1) { // found result or last node, send it
 						SendResult(w, body)
 						return
+					}
+				}
+			}
+		}
+	}
+
+	SendError(w)
+}
+
+func DoAggeratorUriOverHttp_block_search(w http.ResponseWriter, strQuery string) {
+	for i, s := range PoolRpc {
+		rpcUrl := s.Backend.Rpc + "/block_search?" + strQuery
+		body, err := FetchUriOverHttp(rpcUrl)
+		if err == nil {
+			var j0 interface{}
+			err = json.Unmarshal(body, &j0)
+			if err != nil {
+				SendError(w)
+				return
+			}
+
+			if m0, ok := j0.(map[string]interface{}); ok {
+				if result, ok := m0["result"].(map[string]interface{}); ok {
+					if blocks, ok := result["blocks"].([]interface{}); ok {
+						if (len(blocks) > 0) || (i >= len(PoolRpc)-1) { // found result or last node, send it
+							SendResult(w, body)
+							return
+						}
 					}
 				}
 			}
