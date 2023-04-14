d4f8c5e9f1c2c11f6c3cde931810c6a2f14a3b6a
Tuan Pham Anh <baabeetaa@gmail.com>
2023-04-14 16:44:46 +0700

    patch sync_info based on chain-id

--- aggregator.go
+++ aggregator.go
@@ -15,13 +15,29 @@ func DoAggeratorUriOverHttp_status(w http.ResponseWriter, strQuery string) {
 	body, err := FetchUriOverHttp(rpcUrl)
 	if err != nil {
 		SendError(w)
+		return
 	}
 
-	// TODO: fix me, this is hardcode for osmosis
-	body, _ = sjson.SetBytes(body, "result.sync_info.earliest_block_hash", "C8DC787FAAE0941EF05C75C3AECCF04B85DFB1D4A8D054A463F323B0D9459719")
-	body, _ = sjson.SetBytes(body, "result.sync_info.earliest_app_hash", "E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855")
-	body, _ = sjson.SetBytes(body, "result.sync_info.earliest_block_height", "1")
-	body, _ = sjson.SetBytes(body, "result.sync_info.earliest_block_time", "2021-06-18T17:00:00Z")
+	var j0 interface{}
+	err = json.Unmarshal(body, &j0)
+	if err != nil {
+		SendError(w)
+		return
+	}
+
+	if m0, ok := j0.(map[string]interface{}); ok {
+		if result, ok := m0["result"].(map[string]interface{}); ok {
+			if node_info, ok := result["node_info"].(map[string]interface{}); ok {
+				if node_info["network"] == "osmosis-1" {
+					body, _ = sjson.SetBytes(body, "result.sync_info.earliest_block_hash", "C8DC787FAAE0941EF05C75C3AECCF04B85DFB1D4A8D054A463F323B0D9459719")
+					body, _ = sjson.SetBytes(body, "result.sync_info.earliest_app_hash", "E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855")
+					body, _ = sjson.SetBytes(body, "result.sync_info.earliest_block_height", "1")
+					body, _ = sjson.SetBytes(body, "result.sync_info.earliest_block_time", "2021-06-18T17:00:00Z")
+				}
+			}
+		}
+	}
+
 	SendResult(w, body)
 }
 
