1c345906e63cb052124be8d8c1a3469ccfc29f1e
Tuan Pham Anh <baabeetaa@gmail.com>
2023-05-19 01:55:25 +0700

    add Api test (#35)
    add TestApi_cosmos_staking_v1beta1_params

---
+++ test/api_test.go
@@ -0,0 +1,57 @@
+package test
+
+import (
+	sn "github.com/notional-labs/subnode/utils"
+	"github.com/stretchr/testify/suite"
+	"github.com/thedevsaddam/gojsonq/v2"
+	"testing"
+	"time"
+)
+
+func TestApiTestSuite(t *testing.T) {
+	suite.Run(t, new(ApiTestSuite))
+}
+
+type ApiTestSuite struct {
+	suite.Suite
+	UrlEndpoint string
+}
+
+func (s *ApiTestSuite) SetupSuite() {
+	go startServer()
+
+	// wait few secs for the server to init
+	time.Sleep(5 * time.Second)
+
+	s.UrlEndpoint = "http://localhost:1337"
+}
+
+func (s *ApiTestSuite) TearDownSuite() {
+	//server.Shutdown()
+}
+
+func (s *ApiTestSuite) SetupTest() {
+	time.Sleep(1 * time.Second)
+}
+
+func (s *ApiTestSuite) TestApi_cosmos_staking_v1beta1_params() {
+	//{
+	//  "params": {
+	//    "unbonding_time": "1209600s",
+	//    "max_validators": 150,
+	//    "max_entries": 7,
+	//    "historical_entries": 10000,
+	//    "bond_denom": "uosmo",
+	//    "min_commission_rate": "0.050000000000000000",
+	//    "min_self_delegation": "0"
+	//  }
+	//}
+
+	rpcUrl := s.UrlEndpoint + "/cosmos/staking/v1beta1/params"
+	body, err := sn.FetchUriOverHttp(rpcUrl)
+	s.NoError(err)
+
+	v_unbonding_time := gojsonq.New().FromString(string(body)).Find("params.unbonding_time")
+	s.NoError(err)
+	s.True(len(v_unbonding_time.(string)) > 0)
+}
---
+++ test/common.go
@@ -0,0 +1,44 @@
+package test
+
+import (
+	"github.com/notional-labs/subnode/config"
+	"github.com/notional-labs/subnode/server"
+	"github.com/notional-labs/subnode/state"
+	sn "github.com/notional-labs/subnode/utils"
+	"github.com/thedevsaddam/gojsonq/v2"
+)
+
+var isServerStarted = false
+
+func startServer() {
+	if isServerStarted { // already started
+		return
+	}
+
+	isServerStarted = true
+	conf := "../subnode.yaml"
+	c, err := config.LoadConfigFromFile(conf)
+	if err != nil {
+		panic(err)
+	}
+	//fmt.Printf("%+v\n", c)
+	config.SetConfig(c)
+	err = server.Start()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getNetworkName() string {
+	rpcUrl := state.SelectPrunedNode(config.ProtocolTypeRpc).Backend.Rpc + "/status?"
+	body, err := sn.FetchUriOverHttp(rpcUrl)
+	if err != nil {
+		panic(err)
+	}
+	v_network := gojsonq.New().FromString(string(body)).Find("result.node_info.network")
+	network := v_network.(string)
+	if len(network) <= 0 {
+		panic("invalid network")
+	}
+	return network
+}
